37af5fdc5842d448ac0f2c221899a07051d0c63d
fifth_light <liu-yu-jie@foxmail.com>
2025-04-29 22:35:32 +0800

    限制 Github Actions 上 r8 的并行数量

--- buildSrc/src/main/kotlin/r8-parallel.gradle.kts
+++ buildSrc/src/main/kotlin/r8-parallel.gradle.kts
@@ -1,8 +1,10 @@
 import com.gradleup.gr8.Gr8Task
 import top.fifthlight.touchcontoller.gradle.service.GR8BuildingService
 
+val maxR8Tasks = System.getenv("MAX_R8_TASKS")?.toIntOrNull() ?: 2
+
 val gr8BuildingService = project.gradle.sharedServices.registerIfAbsent("gr8", GR8BuildingService::class.java) {
-    maxParallelUsages = 2
+    maxParallelUsages = maxR8Tasks
 }
 
 tasks.withType<Gr8Task> {
